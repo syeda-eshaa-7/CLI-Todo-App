b6c09a7d12df32a31f8291108286a73eeadc5764
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-03-30 17:43:31 -0700

    cli based todo app

---
+++ main.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+let toDo : any =[];
+let condition = true;
+console.log("    WELOCOME TO YOUR TODO APP");
+console.log("*************************************");
+
+while(condition){
+    let task = await inquirer.prompt
+    (
+        [
+            {
+                name : "todo",
+                type : "input",
+                message : "What Do You Want To Add In Your Todo List"
+
+            },
+            {
+                name : "addMore",
+                type : "confirm",
+                default: "false",
+                message : "Do You Want To Add More In Your Todo List"
+            }
+        
+        ]
+    )
+    toDo.push(task.addMore);
+    condition == toDo;
+    console.log(toDo);
+    
+
+}
